0f97dd4741d3e8442d5451758153e420ba8022de
Nayoung.Lee <iny003@naver.com>
2026-02-18 21:40:21 +0900

    [ADD] module 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,5 @@
 import { Module } from '@nestjs/common';
-import { MoviesController } from './movies/movies.controller';
-import { MoviesService } from './movies/movies.service';
+import { MoviesModule } from './movies/movies.module';
 
-@Module({
-  imports: [],
-  controllers: [MoviesController],
-  providers: [MoviesService],
-})
+@Module({ imports: [MoviesModule], controllers: [], providers: [] })
 export class AppModule {}
